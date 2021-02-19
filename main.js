fa201e89364b1932a0eb1464f48bcfc128331bf9
Jorge Nicolas Martin <nicomartin854@gmail.com>
2021-02-19 10:20:32 -0300

    Primer proyecto - Actividad 1 Curso Git

---
+++ main.js
@@ -0,0 +1,40 @@
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+const render = () => {
+    const todosList = document.getElementById("todo-list");
+    const todosTemplate = todos.map((t) => `<li>${t}</li>`);
+    todosList.innerHTML = todosTemplate.join("");
+    const elementos = document.querySelectorAll("#todo-list li");
+    elementos.forEach((elemento, i) => {
+        //leer el envento click
+        elemento.addEventListener("click", () => {
+            elemento.parentNode.removeChild(elemento);
+            todos.splice(i, 1);
+            actualizaTodos(todos);
+            render();
+        });
+    });
+};
+
+const actualizaTodos = (todos) => {
+    const todosString = JSON.stringify(todos);
+    localStorage.setItem("todos", todosString);
+};
+
+window.onload = () => {
+    render();
+    const form = document.getElementById("todo-form");
+    form.onsubmit = (e) => {
+        e.preventDefault();
+        const todo = document.getElementById("todo");
+        console.log(todo);
+        if (!todo.value) {
+            return;
+        }
+        const todoText = todo.value;
+        todo.value = "";
+        todos.push(todoText);
+        actualizaTodos(todos);
+        render();
+    };
+};
